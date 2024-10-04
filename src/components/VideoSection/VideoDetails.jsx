9a863245f05d6423adb60c1c880253c3fa3114d7
arjit88 <arjitanand88@gmail.com>
2024-10-05 02:39:49 +0530

    Make the video response interactive

--- src/components/VideoSection/VideoDetails.jsx
+++ src/components/VideoSection/VideoDetails.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useAppContext } from "../../useContextHook/useContextApi";
+import { useTheme } from "../../useContextHook/useTheme";
+import { fetchApiForYoutubeData } from "../../utils/fetchApi";
 
 const VideoDetails = () => {
-  return <div>VideoDetails</div>;
+  const { categoryId, videoId } = useParams();
+  const { setLoading } = useAppContext();
+  const { isDarkMode } = useTheme();
+
+  const [selectedVideoDetails, setSelectedVideoDetails] = useState();
+  const [channelData, setChannelData] = useState();
+  const [comments, setComments] = useState();
+
+  const fetchSelectedVideoDetails = async () => {
+    setLoading(true);
+    try {
+      const data = await fetchApiForYoutubeData("videos", {
+        part: "snippet,contentDetails,statistics",
+        id: videoId,
+      });
+      setSelectedVideoDetails(data?.items[0]);
+    } catch (error) {
+      console.error(error, "Error fetching selected video");
+    }
+  };
+
+  const fetchChannelData = async () => {
+    if (selectedVideoDetails?.snippet?.channelId) {
+      setLoading(true);
+      try {
+        const data = await fetchApiForYoutubeData(`channels`, {
+          part: "snippet,contentDetails,statistics",
+          id: selectedVideoDetails?.snippet?.channelId,
+        });
+        setChannelData(data?.items[0]);
+      } catch (error) {
+        console.error(error, "Error fetching channel data");
+      } finally {
+        setLoading(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchSelectedVideoDetails();
+  }, [videoId]);
+
+  useEffect(() => {
+    fetchChannelData();
+  }, [selectedVideoDetails]);
+
+  return <div className=""></div>;
 };
 
 export default VideoDetails;
--- src/utils/Loader.jsx
+++ src/utils/Loader.jsx
@@ -1,25 +1,31 @@
 import { useEffect, useState } from "react";
 
-const Loader = () => {
+const Loader = ({ onComplete }) => {
   const [currentProgress, setCurrentProgress] = useState(0);
 
   useEffect(() => {
-    let loader = setInterval(() => {
+    const loader = setInterval(() => {
       setCurrentProgress((prevProgress) => {
-        let newProgress = prevProgress + Math.random() * 40;
-        if (newProgress > 100) newProgress = 100;
-        if (newProgress === 100) clearInterval(loader);
+        let newProgress = prevProgress + Math.random() * 20; // Control increment for smoother experience
+        if (newProgress >= 100) {
+          newProgress = 100;
+          clearInterval(loader);
+          if (onComplete) onComplete(); // Trigger callback when loading is complete
+        }
         return newProgress;
       });
     }, 800);
+
     return () => clearInterval(loader);
-  }, []);
+  }, [onComplete]);
 
   return (
-    <div
-      className="h-1 bg-red-700 transition-all duration-200 absolute z-40 top-0"
-      style={{ width: `${currentProgress}%` }}
-    ></div>
+    <div className="fixed top-0 left-0 w-full z-40">
+      <div
+        className={`h-1 bg-red-700 transition-all duration-200`}
+        style={{ width: `${currentProgress}%` }}
+      />
+    </div>
   );
 };
 
